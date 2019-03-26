a332d797420a4f61aee913391683e1ec4ad138ab
Juuso <a1704528@myy.haaga-helia.fi>
2019-03-26 14:28:00 +0200

    Added RESTFUL service and websecurity

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.CategoryRepository;
@@ -19,6 +23,11 @@ public class BookController {
 
 	@Autowired
 	private CategoryRepository crepository;
+	
+	@RequestMapping(value="/login")
+	public String login() {
+		return "login";
+	}
 	
 	@GetMapping("/")
 	 public String index() {
@@ -60,5 +69,18 @@ public class BookController {
          model.addAttribute("categories", crepository.findAll());
          return "editbook";
     }
+    
+ // RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }       
+
     
 }
